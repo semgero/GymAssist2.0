1f6123232ae65bce33ee3cf307a80fe315e355d7
SebasM-tech <sebashead5@gmail.com>
2025-04-06 17:49:47 -0500

    actualizacion de entitys

--- GymAssist/src/main/java/com/ProyectoAula/GymAssist/models/ClientEntity.java
+++ GymAssist/src/main/java/com/ProyectoAula/GymAssist/models/ClientEntity.java
@@ -1,8 +1,11 @@
 package com.ProyectoAula.GymAssist.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,24 @@ import lombok.NoArgsConstructor;
 @PrimaryKeyJoinColumn(name = "id")
 public class ClientEntity extends UserEntity {
 
-    private String numeroDocumento;
-    private String telefono;
+    @NotBlank
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @NotBlank
+    @Column(name = "mensualidad", nullable = false)
+    private String mensualidad;
+
+    @NotBlank
+    @Column(name = "id_Documento", nullable = false)
+    private String id_Documento;
+
+    @NotBlank
+    @Column(name = "telefono", nullable = false)
+    private int telefono;
+
+    @Email
+    @NotBlank
+    @Column(name = "correo", nullable = false)
+    private String correo;
 }
