b7a85aa1e4775f5ea9a7859e76750aa7d2ae8353
samuel10202 <polo54828@gmail.com>
2025-05-14 11:25:37 -0500

    Refactor: Se eliminaron claves sensibles y se usan variables de entorno

--- GymAssist/src/main/java/com/ProyectoAula/GymAssist/mongoServices/RutinaService.java
+++ GymAssist/src/main/java/com/ProyectoAula/GymAssist/mongoServices/RutinaService.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
